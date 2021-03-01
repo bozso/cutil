ba17dc594caf600912d50bfa7adfcaa571f0722b
István Bozsó <bozso.istvan@csfk.mta.hu>
2021-03-01 11:29:05 +0100

    added error implementations

--- cutil/error/error.h
+++ cutil/error/error.h
@@ -1,4 +1,21 @@
 #ifndef CUTIL_ERROR_H 
 #define CUTIL_ERROR_H
 
+struct Error_t;
+
+struct ErrorMethods {
+    char const*const (*error)(struct Error_t const*const self);
+    void (*drop)(struct Error_t*const self);
+};
+
+struct Error {
+    struct Error_t* self;
+    struct ErrorMethods const* methods;
+};
+
+typedef char const*const (*str_dup_fn)(char const*const);
+void set_str_dup_fn(str_dup_fn const);
+
+struct Error error_format(char const*const fmt, ...);
+
 #endif
