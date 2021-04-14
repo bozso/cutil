8c5e7cce5409d5262ff622811bcb2f912c7b34ac
István Bozsó <bozso.istvan@epss.hu>
2021-04-14 21:17:13 +0200

    added source files too

---
+++ cutil/error/message.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include "error.h"
+
+static char const*const default_str_dup(char const*const in) {
+    char* out = malloc(strlen(in) + 1);
+    out = strcpy(out, in);
+
+    return out;
+}
+
+static str_dup_fn str_dup = default_str_dup;
+
+void set_str_dup_fn(str_dup_fn fn) {
+    str_dup = fn;
+}
+
+
+struct Error_t {
+    char const* msg;
+};
+
+static char const*const error(struct Error_t const*const self) {
+    return str_dup(self->msg);
+}
+
+static void drop(struct Error_t*const self) {
+    free((void*)self->msg);
+    self->msg = NULL;
+}
+
+static struct ErrorMethods const methods = {
+    .error = error,
+    .drop = drop,
+};
+
+struct Error error_format(char const*const fmt, ...) {
+    struct Error err;
+
+    va_list args;
+    va_start(args, fmt);
+    err.self->msg = vsprintf();
+
+
+    va_end(args);
+
+    err.methods = &methods;
+    return err;
+}
---
+++ cutil/error/try.c
@@ -0,0 +1,13 @@
+#include "handling.h"
+
+struct ErrorTry_t {
+    int ntimes;
+    int current;
+};
+
+
+struct ErrorTry try_n_times(int const ntimes) {
+    struct ErrorTry err;
+
+    return err;
+}
